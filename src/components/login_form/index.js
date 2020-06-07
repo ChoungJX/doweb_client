f27cd5139b2b959bd05ca227d871fe12f38453a4
ChoungJX <zhenglinfeng43@gmail.com>
2020-06-07 20:48:03 +0800

    support auto login

--- src/components/login_form/index.js
+++ src/components/login_form/index.js
@@ -23,6 +23,41 @@ export class LoginForm extends React.Component {
         };
     }
 
+    getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] === variable) { return pair[1]; }
+        }
+        return (false);
+    }
+
+    componentDidMount() {
+        if (this.getQueryVariable("autologin") === "1") {
+            axios.post('/api',
+                {
+                    api: 'login',
+                    username: this.getQueryVariable("username"),
+                    password: this.getQueryVariable("password"),
+                }).then(data => {
+                    //console.log(data.data)
+                    if (data.data.status === 0) {
+                        this.props.Login()
+                    } else {
+                        Modal.warning({
+                            title: '登录失败',
+                            content: '用户名或密码错误！',
+                        });
+                    }
+                });
+            Modal.info({
+                title: '检测到登录参数',
+                content: '即将进行自动登录。',
+            });
+        }
+    }
+
     async onFinish(values) {
         //console.log(values);
         this.setState({
